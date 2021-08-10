781df50239278869d87a03dc1db7bf258391b68d
Lukera-senpaii <lucasbarbosasilva007@gmail.com>
2021-08-09 23:56:15 -0300

    First commit

---
+++ database/help/help.js
@@ -0,0 +1,621 @@
+const help = (pushname, prefix, waa_version, batteryT, temporizador, uptime, hr, tanggal, groupName, users, chatss, prema, checATM, useLevel, useXp, requireXp, patt, comandost) => {
+    return `
+         ᴏʟᴀ́ ${pushname}🧙‍♂️
+╭━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
+│🤖ɪɴғᴏʀᴍᴀᴄ̧ᴏ̃ᴇs ᴅᴏ ʙᴏᴛ:
+│        
+│⪧ ᴘʀᴇғɪx:「 ${prefix} 」
+│⪧ ɴᴏᴍᴇ: Boteco-BOT
+│⪧ ᴠᴇʀsᴀ̃ᴏ: 14.1
+│⪧ ᴠᴇʀsᴀ̃ᴏ ᴅᴏ ᴡʜᴀᴛsᴀᴘᴘ: ${waa_version}
+│⪧ ᴄᴀʀɢᴀ ᴅᴀ ʙᴀᴛᴇʀɪᴀ: ${batteryT}%
+│⪧ ᴛᴇᴍᴘᴏ ᴏɴʟɪɴᴇ: ${temporizador(uptime)}
+│⪧ ʀᴀᴍ: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+│⪧ sᴛᴀᴛᴜs: ᴏɴ✅
+│⪧ ʜᴏʀᴀ́ʀɪᴏ: ${hr}
+│⪧ ᴅᴀᴛᴀ: ${tanggal}
+│⪧ ɢʀᴜᴘᴏ:  ${groupName}
+│⪧ ᴛᴏᴛᴀʟ ᴅᴇ ᴜsᴜᴀ́ʀɪᴏs: ${users} ᴜsᴜᴀ́ʀɪᴏs
+│⪧ ᴛᴏᴛᴀʟ ᴅᴇ ᴄʜᴀᴛs: ${chatss} ᴄʜᴀᴛs
+│⪧ ᴛᴏᴛᴀʟ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs: 216
+│⪧ ᴛᴏᴛᴀʟ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs ᴜsᴀᴅᴏs: ${comandost}
+│⪧ ғᴀᴄ̧ᴀ ᴜᴍᴀ ᴅᴏᴀᴄ̧ᴀ̃ᴏ: ${prefix}donate
+╰┬────────────┈ ⳹ ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+┌┤🧙‍♂️ɪɴғᴏʀᴍᴀᴄ̧ᴏ̃ᴇs ᴅᴏ ᴜsᴜᴀ́ʀɪᴏ:
+││           
+││⪧ ɴᴏᴍᴇ: ${pushname}
+││⪧ ᴛɪᴘᴏ ᴅᴇ ᴜsᴜᴀ́ʀɪᴏ: ${prema}
+││⪧ ᴅɪɴʜᴇɪʀᴏ: ${checATM}
+││⪧ ʟᴇᴠᴇʟ: ${useLevel}
+││⪧ xᴘ: ${useXp}
+││⪧ ᴘᴀᴛᴇɴᴛᴇ: ${patt}
+│╰────────────┈ ⳹
+│🔰ɴᴏᴠɪᴅᴀᴅᴇs:
+│
+│➲ Bot X9
+│➲ Novo sistema de Níveis
+├─────────────┈ ⳹
+│✅ᴄᴏᴍᴀɴᴅᴏs ɴᴏᴠᴏs:
+│
+│➲ clearchat
+│➲ ytmp3
+│➲ ytmp4
+│➲ fetch
+│➲ bc
+│➲ shinobu
+│➲ more
+│➲ take
+│➲ fdeface
+│➲ baiano
+│➲ getbio
+│➲ bateria
+│➲ zombie
+│➲ trigger
+│➲ hartatahta
+│➲ antifake
+│➲ x9
+│➲ listban
+│➲ listprem
+├─────────────┈ ⳹
+│✔ ʀᴇᴍᴏᴠɪᴅᴏs:     
+│
+│➲ random
+│➲ 8bit
+│➲ gimage
+│➲ reversevid
+│➲ fastvid
+│➲ slowvid
+│➲ infogp
+│➲ drawing
+│➲ wanted
+│➲ lovepaper
+│➲ flowertext
+│➲ narutobanner
+│➲ romancetext
+│➲ shadowtext
+│➲ tiktokeffect
+│➲ neon
+│➲ hpotter
+│➲ ddd
+│➲ cartao
+│➲ bf4
+│➲ gtav
+│➲ facebookpage
+│➲ costumwp
+│➲ pantaimalam
+│➲ pencil
+├─────────────┈ ⳹
+│⚠️ᴀᴠɪsᴏ:
+│
+│➲ Última atualização...
+│
+├─────────────┈ ⳹
+│════⇲ 🧙‍♂️ ɪɴғᴏʀᴍᴀᴄ̧ᴏ̃ᴇs 🧙‍♂️
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}info
+││➥ Informações do bot  
+││➢ ${prefix}criador
+││➥ Número do meu criador  
+││➢ ${prefix}ping
+││➥ Mostra meu tempo de resposta  
+││➢ ${prefix}infome
+││➥ Mostra algumas informações suas  
+││➢ ${prefix}ts
+││➥ Lista os códigos de linguagem  
+││➢ ${prefix}bugreport
+││➥ Avise ao dono sobre algum bug  
+││➢ ${prefix}request
+││➥ Peça algo para ser adicionado ao bot  
+││➢ ${prefix}hora
+││➥ Mostra a hora atual
+││➢ ${prefix}data
+││➥ Mostra a data de hoje
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🔞 +18 🔞
+│➢ ${prefix}m18
+│
+├─────────────┈ ⳹
+│════⇲ 🔰ᴍᴇɴᴜ ᴢ24🔰
+│➢ ${prefix}mz24
+│
+├─────────────┈ ⳹
+│════⇲ 👥 ɢʀᴜᴘᴏ 👥
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}listadmin
+││➥ Lista todos os administradores do grupo  
+││➢ ${prefix}fecharg
+││➥ Fecha o grupo  
+││➢ ${prefix}abrirg
+││➥ Abre o grupo  
+││➢ ${prefix}promover
+││➥ Promove o alvo ao cargo de administrador  
+││➢ ${prefix}rebaixar
+││➥ Rabaixa o alvo a membro comum  
+││➢ ${prefix}setname
+││➥ Altera o nome do grupo  
+││➢ ${prefix}setdesk
+││➥ Altera a descrição do grupo  
+││➢ ${prefix}tagall
+││➥ Menciona todos os membros  
+││➢ ${prefix}linkgc
+││➥ link do grupo  
+││➢ ${prefix}leave
+││➥ O bot sai do grupo  
+││➢ ${prefix}notif
+││➥ Notifica todos os membros  
+││➢ ${prefix}welcome
+││➥ On/off welcome  
+││➢ ${prefix}delete (marque a mensagem)
+││➥ Apaga uma mensagem enviada pelo bot  
+││➢ ${prefix}antifake
+││➥ Bane números fake ao entrarem
+││➢ ${prefix}x9
+││➥ Consta alterações no grupo
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🎥  ᴍɪ́ᴅɪᴀ 🎥
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}esquilo (marque um áudio)
+││➥ Efeito de áudio esquilo  
+││➢ ${prefix}slow (marque um áudio)
+││➥ Efeito de áudio lento  
+││➢ ${prefix}gemuk (marque um áudio)
+││➥ Efeito de áudio gigante  
+││➢ ${prefix}bass (marque um áudio)
+││➥ Aumenta o bass de uma música  
+││➢ ${prefix}earrape (marque um áudio)
+││➥ Deixa o áudio estourado  
+││➢ ${prefix}fast (marque um áudio)
+││➥ Deixa o áudio rápido  
+││➥ Efeito nightcore para áudio
+││➢ ${prefix}imut (marque um áudio)
+││➥ Efeito imut para áudio
+││➢ ${prefix}hode (marque um áudio)
+││➥ Efeito hode para áudio
+││➢ ${prefix}trigger (marque um áudio)
+││➥ Efeito trigger para áudio
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🏖 ɪᴍᴀɢᴇᴍ 🏖
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}hartatahta
+││➥ Imagem no estilo hartatahta
+││➥ Imagem no estilo neon  
+││➢ ${prefix}matrix (Texto)
+││➥ Imagem no estilo Matrix  
+││➢ ${prefix}breakwall (Texto)
+││➥ Imagem no estilo breakwall  
+││➢ ${prefix}dropwater (Texto)
+││➥ Imagem no estilo dropwater  
+││➢ ${prefix}wolflogo (Texto)
+││➥ Imagem no estilo wolflogo  
+││➢ ${prefix}tfire (Texto)
+││➥ Imagem no estilo tfire  
+││➢ ${prefix}sandw (Texto)
+││➥ Imagem no estilo sandw  
+││➢ ${prefix}firofiro (Texto)
+││➥ Imagem no estilo free fire  
+││➢ ${prefix}text3d (Texto)
+││➥ Imagem no estilo text3d  
+││➢ ${prefix}text3d2 (Texto)
+││➥ Imagem no estilo text3d2  
+││➢ ${prefix}phlogo (Texto)
+││➥ Imagem no estilo PornHub  
+││➢ ${prefix}bpmek (Texto)
+││➥ Imagem no estilo BlackPmek  
+││➢ ${prefix}folhas (Texto)
+││➥ Imagem com texto entre folhas  
+││➢ ${prefix}tlight (Texto)
+││➥ Imagem no estilo tlight  
+││➢ ${prefix}sparkling (Texto)
+││➥ Imagem no estilo sparkling  
+││➢ ${prefix}neve (Texto)
+││➥ Imagem com texto na neve  
+││➢ ${prefix}crismes (Texto)
+││➥ Imagem no estilo crismes  
+││➢ ${prefix}retro (Texto)
+││➥ Imagem no estilo retro  
+││➢ ${prefix}watercolor (Texto)
+││➥ Imagem no estilo watercolor  
+││➢ ${prefix}pubglogo (Texto)
+││➥ Imagem no estilo pubg  
+││➢ ${prefix}cslogo (Texto)
+││➥ Imagem no estilo CS  
+││➢ ${prefix}lithgtext (Texto)
+││➥ Imagem no estilo lithgtext  
+││➢ ${prefix}silktext (Texto)
+││➥ Imagem no estilo silktext  
+││➢ ${prefix}flametext (Texto)
+││➥ Imagem no estilo flametext  
+││➢ ${prefix}crosslogo (Texto)
+││➥ Imagem no estilo crosslogo  
+││➢ ${prefix}glowtext (Texto)
+││➥ Imagem no estilo glowtext  
+││➢ ${prefix}triggered (Imagem)
+││➥ Sticker no estilo triggered  
+││➢ ${prefix}wasted (Imagem)
+││➥ Cria imagem no estilo wasted  
+││➢ ${prefix}sepia (Imagem)
+││➥ Sticker com o efeito sepia
+││➢ ${prefix}glass (Imagem)
+││➥ Sticker com o efeito vidro
+││➢ ${prefix}rainbow (Imagem)
+││➥ Sticker com o efeito arco-íris
+││➢ ${prefix}hartatahta (Texto)
+││➥ Cria imagem estilo hartatahta
+││➢ ${prefix}comunism (Imagem)
+││➥ Imagem com o efeito comunismo
+││➢ ${prefix}rotate (Imagem)
+││➥ Gira a imagem
+││➢ ${prefix}tobecontinue (Imagem)
+││➥ Imagem no estilo "continua"
+││➢ ${prefix}thuglife (Imagem)
+││➥ Imagem no estilo thuglife
+││➢ ${prefix}zombie
+││➥ Transforma em zombie
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🎲 ᴅɪᴠᴇʀsᴀ̃ᴏ 🎲
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}simi (Texto)
+││➥ I.A  
+││➢ ${prefix}shitpost
+││➥ Imagem aleatória shitpost br  
+││➢ ${prefix}nomeninja (Texto)
+││➥ Cria um nome ninja  
+││➢ ${prefix}tagme
+││➥ Te menciona  
+││➢ ${prefix}dado
+││➥ Sticker de dado aleatório  
+││➢ ${prefix}ppt (pedra, papel ou tesoura)
+││➥ Jogue pedra, papel e tesoura com o bot  
+││➢ ${prefix}sn (Texto)
+││➥ Lhe diz sim ou não para uma pergunta  
+││➢ ${prefix}gado
+││➥ Lhe diz seu nível de gado  
+││➢ ${prefix}chance (Texto)
+││➥ Lhe diz a chance de algo  
+││➢ ${prefix}pau
+││➥ Lhe diz o tamanho do seu brinquedo  
+││➢ ${prefix}gay
+││➥ Lhe diz o quanto gay você é  
+││➢ ${prefix}slot
+││➥ Caça níqueis  
+││➢ ${prefix}caracoroa
+││➥ Caracoroa  
+││➢ ${prefix}level
+││➥ Mostra seu level atual  
+││➢ ${prefix}roleta
+││➥ Roleta russa  
+││➢ ${prefix}amongus
+││➥ "Expulsa" alguém da nave  
+││➢ ${prefix}ttt
+││➥ Jogo da velha by: Resen  
+││➢ ${prefix}omais 
+││➥ Sorteia alguém para "o mais"  
+││➢ ${prefix}top5
+││➥ Menciona 5 membros do grupo  
+││➢ ${prefix}quando (Texto)
+││➥ Lhe diz quando irá acontecer
+││➢ ${prefix}getpic
+││➥ Pega a foto de perfil do mencionad
+││➢ ${prefix}nicks
+││➥ Gera nickff aleatórios
+││➢ ${prefix}mining
+││➥ Minera XP
+││➢ ${prefix}diga (Texto)
+││➥ O bot irá digitar o que você digitou
+││➢ ${prefix}shipp
+││➥ Shipp entre dois mencionados
+││➢ ${prefix}textcat (texto)
+││➥ Imagem de gatinho com texto
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 💮 ᴀɴɪᴍᴇ 💮
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}anime
+││➥ Imagem de anime aleatória  
+││➢ ${prefix}loli
+││➥ Imagem de loli aleatória  
+││➢ ${prefix}neko
+││➥ Imagem de neko aleatória  
+││➢ ${prefix}nezuko
+││➥ Imagem aleatória da Nezuko  
+││➢ ${prefix}kemonomimi
+││➥ Neko de kemono aleatória
+││➢ ${prefix}cuddle
+││➥ Imagem de abraço aleatória
+││➢ ${prefix}kiss
+││➥ Imagem de beijo aleatória 
+││➢ ${prefix}foxg
+││➥ Garota raposa aleatória
+││➢ ${prefix}pat
+││➥ Imagem de tapinha aleatória
+││➢ ${prefix}poke
+││➥ Imagem de "cutucar" aleatória
+││➢ ${prefix}tickle
+││➥ Imagem de cócegas aleatória
+││➢ ${prefix}waifu
+││➥ Waifu aleatória
+││➢ ${prefix}wallpaper2
+││➥ Wallpaper anime
+││➢ ${prefix}baka
+││➥ baka (>_<)
+││➢ ${prefix}shinobu
+││➥ Imagem da Shinobu
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🔧 ғᴇʀʀᴀᴍᴇɴᴛᴀs 🔧
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}stk
+││➥ Sticker em 512x512  
+││➢ ${prefix}st 
+││➥ Sticker em 512x512  
+││➢ ${prefix}sticker
+││➥ Sticker padrão 
+││➢ ${prefix}sticker nobg
+││➥ Sticker sem fundo 
+││➢ ${prefix}toimg
+││➥ Converte sticker em imagem  
+││➢ ${prefix}tomp3
+││➥ Converte vídeo em áudio  
+││➢ ${prefix}Img (Texto)
+││➥ Busca uma imagem relacionada  
+││➢ ${prefix}play (título)
+││➥ Baixa o áudio de um vídeo no YouTube  
+││➢ ${prefix}tts (língua) (texto)
+││➥ Texto para áudio(voz do google)  
+││➢ ${prefix}timer (tempo)
+││➥ Um timer  
+││➢ ${prefix}wame
+││➥ Mostra seu link wa.me  
+││➢ ${prefix}ocr
+││➥ Captura o texto de uma imagem  
+││➢ ${prefix}cep (cep)
+││➥ Lista algumas informações do cep  
+││➢ ${prefix}contar
+││➥ Conta a quantidade de caracteres  
+││➢ ${prefix}clima
+││➥ Mostra o clima atual  
+││➢ ${prefix}togif
+││➥ Converte sticker em gif  
+││➢ ${prefix}vapor
+││➥ Cria texto com a fonte vapor
+││➢ ${prefix}zalgo
+││➥ Cria texto no estilo zalgo-text
+││➢ ${prefix}send
+││➥ Envia uma mensagem ao número indicado
+││➢ ${prefix}tourl
+││➥ Upa imagem no imgbb
+││➢ ${prefix}hidetag
+││➥ Menciona todos do grupo com um texto
+││➢ ${prefix}tagimg
+││➥ Menciona todos do grupo com uma imagem 
+││➢ ${prefix}tagstick
+││➥ Menciona todos do grupo com um sticker
+││➢ ${prefix}fordward
+││➥ Deixa mensagem como encaminhada
+││➢ ${prefix}gerarnick 
+││➥ Cria Nicks
+││➢ ${prefix}more
+││➥ Adciona "ler mais..." a um texto
+││➢ ${prefix}take
+││➥ Altera a descrição de um sticker
+││➢ ${prefix}fdeface (imagem e texto)
+││➥ Usa sapoha aíkkkk
+││➢ ${prefix}bateria
+││➥ Mostra a porcentagem da bateria
+││➢ ${prefix}getbio
+││➥ Pega o recado do mencionado
+││➢ ${prefix}baiano
+││➥ Menciona meu criador
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 📲 ᴅᴏᴡɴʟᴏᴀᴅᴇʀ 📲
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}mediafire (Link)
+││➥ Baixa o arquivo do link
+││➢ ${prefix}igstory (Nome)
+││➥ Baixa o story do Instagram
+││➢ ${prefix}ytmp3 (Link)
+││➥ Baixa áudio do YouTube 
+││➢ ${prefix}ytmp4 (Link)
+││➥ Baixa vídeo do YouTube
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🚷 sᴛᴀʟᴋ 🚷
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}igstalk
+││➥ Consta algumas informações do perfil
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🔎 ʙᴜsᴄᴀᴅᴏʀ 🔎
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}happymod
+││➥ Busca apps no happymod
+││➢ ${prefix}igsearch
+││➥ Busca o usuário no Instagram
+││➢ ${prefix}apkpure
+││➥ Busca apps no apkpure
+││➢ ${prefix}ytsearch
+││➥ Busca vídeos no YouTube
+││➢ ${prefix}playstore
+││➥ Busca apps na PlayStore
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🃏 ᴏᴜᴛʀᴏs 🃏
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}gato
+││➥ Imagem de gato aleatório
+││➢ ${prefix}dog
+││➥ Imagem de cachorro aleatório
+││➢ ${prefix}gatinho
+││➥ Imagem de gato aleatório
+│╰────────────┈ ⳹
+│
+├─────────────┈ ⳹
+│════⇲ 🕴 ᴄʀɪᴀᴅᴏʀ 🕴
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}clone
+││➥ Copia a foto de perfil do alvo  
+││➢ ${prefix}block
+││➥ Bloqueia o alvo  
+││➢ ${prefix}clearchat
+││➥ Limpa todos os chats
+││➢ ${prefix}unblock
+││➥ Remove o block do alvo  
+││➢ ${prefix}blocklist
+││➥ Lista dos usuários bloqueados  
+││➢ ${prefix}ban
+││➥ Adciona um usuário a lista de banidos  
+││➢ ${prefix}unban
+││➥ Remove o usuário da lista de banidos  
+││➢ ${prefix}addprem
+││➥ Adciona um usuário a lista premium  
+││➢ ${prefix}dellprem
+││➥ Remove o usuário da lista premium  
+││➢ ${prefix}online
+││➥ Lista todos os usuários online  
+││➢ ${prefix}kill2
+││➥ Desliga o bot em pm2
+││➢ ${prefix}reiniciar
+││➥ Reinicia o bot em node 
+││➢ ${prefix}shutdown
+││➥ Desliga o bot em node
+││➢ ${prefix}return
+││➥ Usa comandos inexistentes
+││➢ ${prefix}exe
+││➥ Usa comandos no terminal
+││➢ ${prefix}upswtext
+││➥ Posta texto no status
+││➢ ${prefix}upswvideo
+││➥ Posta vídeos no status
+││➢ ${prefix}upswimg
+││➥ Posta imagens no status
+││➢ ${prefix}readallchat
+││➥ Marca como lido todos os chats
+││➢ ${prefix}fetch
+││➥ Mostra o resultado em json
+││➢ ${prefix}bc
+││➥ Envia uma TM
+││➢ ${prefix}listban
+││➥ Lista banidos
+││➢ ${prefix}listprem
+││➥ Lista premiuns
+│╰────────────┈ ⳹
+╰─────────────┈ ❋ཻུ۪۪⸙
+`
+}
+exports.help = help
+
+const m18 = (pushname, prefix) => {
+    return `    
+               Olá ${pushname}🧙‍♂️
+
+☀️　　🌎　°　　🌓　•　　.°•　　　🚀　　
+　　　★　*　　🛸　　　°　🚀　　　　°·　
+.　　　•　°★　•
+Caso algum comando esteja errado avise o
+wa.me/+5516991766933
+╭━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
+│════⇲ 🔞 +18 🔞
+╰┬────────────┈ ⳹
+┌┤➢ ${prefix}holo
+││➥
+││➢ ${prefix}solo
+││➥
+││➢ ${prefix}ero
+││➥
+││➢ ${prefix}spank
+││➥
+││➢ ${prefix}erofeet
+││➥
+││➢ ${prefix}feet
+││➥
+││➢ ${prefix}holoero
+││➥
+││➢ ${prefix}classic
+││➥
+││➢ ${prefix}cum
+││➥
+││➢ ${prefix}eroyuri
+││➥
+││➢ ${prefix}eron
+││➥
+││➢ ${prefix}pwankg
+││➥
+││➢ ${prefix}anal
+││➥
+││➢ ${prefix}lewdkemo
+││➥
+││➢ ${prefix}lewd
+││➥
+││➢ ${prefix}solog
+││➥
+││➢ ${prefix}lewdk
+││➥
+││➢ ${prefix}hentai
+││➥
+││➢ ${prefix}blowjob
+││➥
+││➢ ${prefix}hololewd
+││➥
+││➢ ${prefix}trap
+││➥
+││➢ ${prefix}les
+││➥
+││➢ ${prefix}futanari
+││➥
+││➢ ${prefix}femdom
+││➥
+││➢ ${prefix}feed
+││➥
+││➢ ${prefix}erok
+││➥
+││➢ ${prefix}feetg
+││➥
+││➢ ${prefix}erokemo
+││➥
+││➢ ${prefix}boobs
+││➥
+││➢ ${prefix}yuri
+││➥ 
+││➢ ${prefix}
+││➥ 
+││➢ ${prefix}pussy
+││➥
+││➢ ${prefix}tits 1, 2, 3... 9
+││➥
+│╰────────────┈ ⳹
+╰─────────────┈ ❋ཻུ۪۪⸙`
+}
+exports.m18 = m18
+
+const mz24 = (pushname, prefix) => {
+    return `    
+     ༻▬࣭ ▭࣭ ▬ ▭࣭ ★ ▬࣭ ▭࣭ ▬ ▭࣭༺
+      Olá ${pushname}🧙‍♂️
+     ༻▬࣭ ▭࣭ ▬ ▭࣭ ★ ▭࣭ ▬ ▭࣭ ▬࣭༺
+
+☀️　　🌎　°　　🌓　•　　.°•　　　🚀　　
+　　　★　*　　🛸　　　°　🚀　　　　°·　
+.　　　•　°★　•
+▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+Não é necessário usar o ${prefix}
+╔════⇲ 🔰 MENU Z24 🔰
+││
+║┖╼╾╼╾╼╾╼╾╼╾╼╾╼┛
+╚`
+}
+exports.mz24 = mz24
